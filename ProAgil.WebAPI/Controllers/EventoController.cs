bf71c0faf4ec195a65e82b6cc2db87dd7c9cbe8d
maxwell morais <mg_morais@id.uff.br>
2020-01-29 00:10:34 -0300

    async methods

--- ProAgil.WebAPI/Controllers/EventoController.cs
+++ ProAgil.WebAPI/Controllers/EventoController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ProAgil.WebAPI.Models;
 
@@ -12,27 +14,33 @@ namespace ProAgil.WebAPI.Controllers
     [Route("[controller]")]
     public class EventoController: ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<Evento>> Get()
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
-            return new Evento[] {
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e Net Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1 Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular e suas novidades",
-                    Local = "São Paulo",
-                    Lote = "2 Lote",
-                    QtdPessoas = 450,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
-            };
+            this._context = context;
+            
+        }
+        
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var result = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(result);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro no servidor");
+            }
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Evento>>> Get()
+        {
+            return await _context.Eventos.ToListAsync();
         }
     }
 }
